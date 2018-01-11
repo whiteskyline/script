9296a20ab0066c57045c5e41c74fe18d80f5b206
linmingxing <linmingxing@bytedance.com>
2018-01-11 14:20:29 +0800

    update: init all scripts.

---
+++ utils/dev_machine_script.sh
@@ -0,0 +1,29 @@
+#
+# 支持开发机上相关的服务的启动
+#
+
+function dev_start() {
+	BASE_DIR=~/repos/toutiao/aweme
+	if [[ $1 == "api" ]]; then
+		cd $BASE_DIR && runtime/bin/python app/api/bootstrap.py 9310
+	elif [[ $1 == "notice" ]]; then
+		cd $BASE_DIR && runtime/bin/python app/service/notice/bootstrap.py
+	elif [[ $1 == "shell" ]]; then
+		if [[ $# -gt 2 ]]; then
+			echo "maybe these scripts will help you."
+			cat ~/py_scripts/$3
+		fi
+		if [[ $2 == "api" ]]; then
+			cd $BASE_DIR && runtime/bin/python app/api/manage.py shell
+		elif [[ $2 == "notice" ]]; then
+			cd $BASE_DIR && runtime/bin/python app/service/notice/manage.py shell
+		fi
+	fi
+}
+
+function dev_go() {
+	BASE_DIR=~/repos/toutiao/aweme
+	if [[ $1 == "logs" ]]; then
+		cd $BASE_DIR/log
+	fi
+}
