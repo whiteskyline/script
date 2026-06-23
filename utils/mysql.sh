d8171f78598d1526e7294cd4d90e32f2a56d64c7
linmingxing <linmingxing@bytedance.com>
2026-06-24 02:00:33 +0800

    update: 初步整理.

--- utils/mysql.sh
+++
@@ -1,6 +0,0 @@
-# MySQL 本地命令。
-MYSQL_HOME=$HOME_DIR/soft/package/mysql
-MYSQL_CONF_HOME=$HOME_DIR/soft/config/mysql
-alias mysqld_safe="cd $MYSQL_HOME && nohup $MYSQL_HOME/bin/mysqld_safe --defaults-file=$MYSQL_CONF_HOME/my.cnf & > /tmp/mysql.start.log"
-alias mysqladmin="$MYSQL_HOME/bin/mysqladmin -uroot -p"
-alias mysql="$MYSQL_HOME/bin/mysql"
